1f1e5fe76d8b2902e1015d636f5f4f2f60a513c1
Luciano Oliveira <luciano8.oliveira@gmail.com>
2025-03-08 15:16:16 +0000

    change icons

--- website/config.py
+++ website/config.py
@@ -1,5 +1,9 @@
 class Config:
     TITLE = "Event Sponsor Hub"
-    FAVICON = "../static/images/favicon.ico"
+    # FAVICON = "../static/images/favicon.ico"
     # FAVICON = "../static/images/newIcon.ico"
-    LOGOICON = "../static/images/logo-icon.png"
+    # LOGOICON = "../static/images/logo-icon.png"
+
+
+    FAVICON = "../static/images/esh icon/favicon.ico"
+    LOGOICON = "../static/images/esh icon/EventSponsorHub.png"
